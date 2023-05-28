0f9b868880e1ea01a369a716b9d15b0475b1041e
BooyahSquadians69 <jiveshkalra69@gmail.com>
2023-05-28 09:33:32 +0530

    login/register btns

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -3,20 +3,52 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Str;
 
 class MainController extends Controller
 {
-    public function index(){
+    public function index()
+    {
         return view('index');
     }
-    
-    public function register(Request $request){
+
+    public function register(Request $request)
+    {
         $request->validate(
             [
                 'email' => 'required|email',
             ]
         );
-        echo "<pre>";
-        print_r($request->all());
+        
+        $apiUrl = 'https://randomuser.me/api/';
+
+        // Make the API request
+        $response = Http::get($apiUrl);
+
+        // Check if the response is successful
+        if ($response->successful()) {
+            // Get the JSON response body
+            $data = $response->json();
+
+            // Check if the "results" key exists and has at least one element
+            if (isset($data['results']) && count($data['results']) > 0) {
+                $results = $data['results'];
+                $nameArray = $results[0]['name'];
+
+                // Merge the last two names in the array
+                $username = implode(' ', array_slice($nameArray, -2));
+
+                // Display the merged names
+            $password = Str::random(10); // Generate a random password with 10 characters
+
+            } else {
+                echo "No results found.";
+            }
+        } else {
+            echo "Failed to fetch data from the API.";
+        }
+        $data = compact('username','password');
+        return view('showuser')->with($data);
     }
 }
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,18 +1,22 @@
 @extends('layouts.main')
 
 @section('main-content')
+<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
     <link rel="stylesheet" href="src/css/index.css">
     <div class="content-container">
         <div class="grid place-items-center w-full"> {{-- m-4 sm:m-8 md:m-12 lg:m-20 --}}
             <img class="w-96 h-auto" src="src/img/counsellor.png" alt="Counsellor.png">
             <a style="display:none;" href="https://storyset.com/people">People illustrations by Storyset</a>
             <h1 class="text-lg md:text-xl lg:text-2xl font-semibold lg:font-bold px-10 heading my-5 text-blue-700">
-                Empathy in Anonymity: Reach Out for Student Wellness</h1>
+                Empathy in Anonymity: Reach Out for Student Wellness
+            </h1>
         </div>
         <div class="lower-box">
             <form class="msg-inputarea w-screen" action="">
                 <textarea rows="1" type="text" id="msg-input" class="msg-input" placeholder="Enter your message..."></textarea>
-                <button type="submit" class="msg-send-btn">Send</button>
+                <button type="submit" class="msg-send-btn"><span class="material-symbols-outlined">
+                    send
+                    </span></button>
             </form>
         </div>
     </div>
