7ebcd9d77b4b2dadb7fe7ce5339fe5331585ee61
BooyahSquadians69 <jiveshkalra69@gmail.com>
2023-05-31 22:19:45 +0530

    Revert "mistake"
    This reverts commit 7de6f4475bc88998bdfbaf45c7329e23f553b63b.

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,9 @@
-import './bootstrap';
+import 'flowbite';
+import { createApp } from 'vue';
+import user_modal from "./user-pass-modal.vue";
+import error_modal from "./error-modal.vue";
+import loggedin_modal from "./loggedin-modal.vue";
 
-import Alpine from 'alpinejs';
-
-window.Alpine = Alpine;
-
-Alpine.start();
+createApp(user_modal).mount("#user-pass-modal");
+createApp(error_modal).mount("#error_modal");
+createApp(loggedin_modal).mount("#loggedin_modal");
