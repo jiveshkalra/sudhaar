06ef149062295c14455e1d9b6983a84ea08440db
BooyahSquadians69 <jiveshkalra69@gmail.com>
2023-06-09 16:35:18 +0530

    fked up

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,14 +1,7 @@
-import 'flowbite';
-import { createApp } from 'vue';
-import user_modal from "./user-pass-modal.vue";
-import error_modal from "./error-modal.vue";
-import loggedin_modal from "./loggedin-modal.vue";
-import register_modal from "./register-modal.vue";
-import tologin_modal from "./tologin-modal.vue";
+import './bootstrap';
 
-createApp(tologin_modal).mount("#tologin_modal");
-createApp(user_modal).mount("#user-pass-modal");
-createApp(register_modal).mount("#register-modal");
-createApp(error_modal).mount("#error_modal");
-createApp(loggedin_modal).mount("#loggedin_modal");
+import Alpine from 'alpinejs';
 
+window.Alpine = Alpine;
+
+Alpine.start();
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -73,7 +73,7 @@
                     </ul>
                 </div>
             </div>
-        </nav>
+            </header>
 
         <main class="py-4">
             @yield('content')
